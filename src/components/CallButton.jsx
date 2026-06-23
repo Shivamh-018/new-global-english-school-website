1c36fd25b691902121f906b72285a4a918ba8ef7
Shivam Honrao <shivamhonrao7@gmail.com>
2026-06-23 13:12:06 +0530

    Updated call button number

--- src/components/CallButton.jsx
+++ src/components/CallButton.jsx
@@ -3,7 +3,7 @@ import { FaPhoneAlt } from "react-icons/fa";
 function CallButton() {
   return (
     <a
-      href="tel:8805425262"
+      href="tel:9370671599"
       className="fixed bottom-24 right-5 bg-blue-600 w-16 h-16 rounded-full flex items-center justify-center text-white text-2xl shadow-xl hover:scale-110 transition z-50"
     >
       <FaPhoneAlt />
